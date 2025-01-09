14ffe4b8e88e8e44a32b6bc0b6e528720a48e4d2
rodrigosalles7 <rodrigo.oliveira@scoder.com.br>
2025-01-09 11:14:19 -0300

    add ramp-up, constant and ramp-down

--- performance/loadTest.js
+++ performance/loadTest.js
@@ -1,17 +1,36 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check, sleep } from 'k6';
 
 const BASE_URL = __ENV.BASE_URL || 'https://api-hml2.ilotto.com.br/bff/customer/winners-feed?orderBy=date&skip=0&take=10';
-const VIRTUAL_USERS = __ENV.VIRTUAL_USERS || '30000';
-const TEST_DURATION = __ENV.TEST_DURATION || '1h';
+const VIRTUAL_USERS_CONSTANT = __ENV.VIRTUAL_USERS_CONSTANT || 100;
+const VIRTUAL_USERS_RAMP_UP = __ENV.VIRTUAL_USERS_RAMP_UP || 100;
+const VIRTUAL_USERS_RAMP_DOWN = __ENV.VIRTUAL_USERS_RAMP_DOWN || 0;
+const TEST_DURATION_CONSTANT = __ENV.TEST_DURATION_CONSTANT || '5m';
+const TEST_DURATION_RAMP_UP = __ENV.TEST_DURATION_RAMP_UP || '1h';
+const TEST_DURATION_RAMP_DOWN = __ENV.TEST_DURATION_RAMP_DOWN || '5';
 
-// Configuração de carga com base nas variáveis
+// Configuração de carga
 export const options = {
-	vus: parseInt(VIRTUAL_USERS),
-	duration: TEST_DURATION,
+	stages: [
+		{ duration: TEST_DURATION_RAMP_UP, target: VIRTUAL_USERS_RAMP_UP },  // Ramp-up para 50 usuários em 1 minuto
+		{ duration: TEST_DURATION_CONSTANT, target: parseInt(VIRTUAL_USERS_CONSTANT) },  // Mantém os usuários virtuais especificados
+		{ duration: TEST_DURATION_RAMP_DOWN, target: VIRTUAL_USERS_RAMP_DOWN },  // Ramp-down para 0 usuários em 1 minuto
+	],
+	thresholds: {
+		http_req_duration: ['p(95)<500'], // 95% das requisições devem ser menores que 500ms
+		http_req_failed: ['rate<0.01'],   // Taxa de falhas < 1%
+	},
 };
 
 export default function () {
-	http.get(BASE_URL);
+	const res = http.get(BASE_URL);
+
+	// Validações para garantir qualidade da resposta
+	check(res, {
+		'status é 200': (r) => r.status === 200,
+		'tempo de resposta < 500ms': (r) => r.timings.duration < 500,
+	});
+
+	// Simula intervalo entre as requisições
 	sleep(1);
 }

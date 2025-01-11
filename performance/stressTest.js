f0a8e616b2c342cfd5bfa1e820422bbc8f7ee7f4
rodrigosalles7 <rodrigo.oliveira@scoder.com.br>
2025-01-11 13:40:11 -0300

    create stress tests

--- performance/stressTest.js
+++ performance/stressTest.js
@@ -1,14 +1,38 @@
-// Stress
+import http from 'k6/http';
+import { sleep } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+const BASE_URL = __ENV.BASE_URL || 'https://api-hml2.ilotto.com.br';
+const STRESS_VUS_LOW = __ENV.STRESS_VUS_LOW || 100;
+const STRESS_VUS_MEDIUM = __ENV.STRESS_VUS_MEDIUM || 200;
+const STRESS_VUS_HIGH = __ENV.STRESS_VUS_HIGH || 300;
+const STRESS_VUS_MAX = __ENV.STRESS_VUS_MAX || 400;
+const STRESS_DURATION_SHORT = __ENV.STRESS_DURATION_SHORT || '2m';
+const STRESS_DURATION_LONG = __ENV.STRESS_DURATION_LONG || '5m';
+const STRESS_RAMP_DOWN = __ENV.STRESS_RAMP_DOWN || '10m';
+
 export const options = {
-	stages: [
-		{ duration: "2m", target: 100 },
-		{ duration: "5m", target: 100 },
-		{ duration: "2m", target: 200 },
-		{ duration: "5m", target: 200 },
-		{ duration: "2m", target: 300 },
-		{ duration: "5m", target: 300 },
-		{ duration: "2m", target: 400 },
-		{ duration: "5m", target: 400 },
-		{ duration: "10m", target: 0 },
-	],
+  stages: [
+    { duration: STRESS_DURATION_SHORT, target: STRESS_VUS_LOW },
+    { duration: STRESS_DURATION_LONG, target: STRESS_VUS_LOW },
+    { duration: STRESS_DURATION_SHORT, target: STRESS_VUS_MEDIUM },
+    { duration: STRESS_DURATION_LONG, target: STRESS_VUS_MEDIUM },
+    { duration: STRESS_DURATION_SHORT, target: STRESS_VUS_HIGH },
+    { duration: STRESS_DURATION_LONG, target: STRESS_VUS_HIGH },
+    { duration: STRESS_DURATION_SHORT, target: STRESS_VUS_MAX },
+    { duration: STRESS_DURATION_LONG, target: STRESS_VUS_MAX },
+    { duration: STRESS_RAMP_DOWN, target: 0 },
+  ],
 };
+
+export default function () {
+  const url = (BASE_URL);
+  http.get(url);
+  sleep(1);
+}
+
+export function handleSummary(data) {
+  return {
+    "./reports/indexStress.html": htmlReport(data),
+  };
+}

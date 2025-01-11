487d3f6a71efb7aadd9ad093533e83f5e169fdd8
rodrigosalles7 <rodrigo.oliveira@scoder.com.br>
2025-01-11 13:27:57 -0300

    create spike tests

--- performance/spikeTest.js
+++ performance/spikeTest.js
@@ -1,20 +1,35 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
-// Spike
+const BASE_URL = __ENV.BASE_URL || 'https://api-hml2.ilotto.com.br';
+const SPIKE_VUS_HIGH = __ENV.SPIKE_VUS_HIGH || 1400;
+const SPIKE_VUS_LOW = __ENV.SPIKE_VUS_LOW || 100;
+const SPIKE_DURATION_SHORT = __ENV.SPIKE_DURATION_SHORT || '10s';
+const SPIKE_DURATION_LONG = __ENV.SPIKE_DURATION_LONG || '3m';
+const SPIKE_RAMP_UP = __ENV.SPIKE_RAMP_UP || '1s';
+const SPIKE_RAMP_DOWN = __ENV.SPIKE_RAMP_DOWN || '10s';
+
 export const options = {
-	stages: [
-		{ duration: "1s", target: 1000 },
-		// { duration: "1m", target: 100 },
-		// { duration: "10s", target: 1400 },
-		// { duration: "3m", target: 1400 },
-		// { duration: "10s", target: 100 },
-		// { duration: "3m", target: 100 },
-		// { duration: "10s", target: 0 },
-	],
+  stages: [
+    { duration: SPIKE_RAMP_UP, target: SPIKE_VUS_HIGH },  // Ramp-up rápido
+    { duration: SPIKE_DURATION_LONG, target: SPIKE_VUS_LOW },  // Mantém carga baixa
+    { duration: SPIKE_DURATION_SHORT, target: SPIKE_VUS_HIGH },  // Spike para carga alta
+    { duration: SPIKE_DURATION_LONG, target: SPIKE_VUS_HIGH },  // Mantém carga alta
+    { duration: SPIKE_RAMP_DOWN, target: SPIKE_VUS_LOW },  // Ramp-down para carga baixa
+    { duration: SPIKE_DURATION_LONG, target: SPIKE_VUS_LOW },  // Mantém carga baixa
+    { duration: SPIKE_RAMP_DOWN, target: 0 },  // Finaliza o teste
+  ],
 };
 
-export default () => {
-	const urlRes = http.get('https://api-hml2.ilotto.com.br/bff/customer/winners-feed?orderBy=date&skip=0&take=10');
-	sleep(1);
-};
+export default function () {
+  const url = (BASE_URL);
+  http.get(url);
+  sleep(1);
+}
+
+export function handleSummary(data) {
+  return {
+    "./reports/indexSpike.html": htmlReport(data),
+  };
+}

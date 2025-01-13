66ec920e713b8c2f6a914ce3eb4ea75e074e5e36
rodrigosalles7 <rodrigo.oliveira@scoder.com.br>
2025-01-13 11:37:31 -0300

    create POST request

--- performance/loadTest.js
+++ performance/loadTest.js
@@ -2,11 +2,11 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
-const BASE_URL = __ENV.BASE_URL || 'https://hml2.ilotto.com.br';
-const REQUEST_TYPE = __ENV.REQUEST_TYPE || 'GET'; // Variável de ambiente para GET ou POST
-const POST_BODY = __ENV.POST_BODY || '{}'; // Corpo da requisição POST, em formato JSON
-const VIRTUAL_USERS_CONSTANT = __ENV.VIRTUAL_USERS_CONSTANT || 10;
-const VIRTUAL_USERS_RAMP_UP = __ENV.VIRTUAL_USERS_RAMP_UP || 10;
+const BASE_URL = __ENV.BASE_URL || 'https://api-hml2.ilotto.com.br/carts/purchase';
+const REQUEST_TYPE = __ENV.REQUEST_TYPE || 'POST'; // Variável de ambiente para GET ou POST
+const POST_BODY = __ENV.POST_BODY || '{"raffleId":"b5af8b36-015e-4d1b-a69d-ac2e22f5f663","ticketsAmount":10000,"customerId":"","name":"Rodrigo O. S.","email":"rodrigo.oliveira@scoder.com.br","cpf":"12622755635","phone":"5535992027336","birthDate":null,"purchaseWithPromotion":false}'; // Corpo da requisição POST, em formato JSON
+const VIRTUAL_USERS_CONSTANT = __ENV.VIRTUAL_USERS_CONSTANT || 1;
+const VIRTUAL_USERS_RAMP_UP = __ENV.VIRTUAL_USERS_RAMP_UP || 1;
 const VIRTUAL_USERS_RAMP_DOWN = __ENV.VIRTUAL_USERS_RAMP_DOWN || 0;
 const TEST_DURATION_CONSTANT = __ENV.TEST_DURATION_CONSTANT || '5s';
 const TEST_DURATION_RAMP_UP = __ENV.TEST_DURATION_RAMP_UP || '10s';
@@ -20,7 +20,7 @@ export const options = {
 		{ duration: TEST_DURATION_RAMP_DOWN, target: VIRTUAL_USERS_RAMP_DOWN },
 	],
 	thresholds: {
-		http_req_duration: ['p(95)<500'],
+		http_req_duration: ['p(95)<1000'],
 		http_req_failed: ['rate<0.01'],
 	},
 };
@@ -30,17 +30,18 @@ export default function () {
 
 	if (REQUEST_TYPE.toUpperCase() === 'POST') {
 		res = http.post(BASE_URL, POST_BODY, { headers: { 'Content-Type': 'application/json' } });
+		check(res, {
+			'status é 201': (r) => r.status === 201,
+			'tempo de resposta < 1000ms': (r) => r.timings.duration < 1000,
+		});
 	} else {
 		res = http.get(BASE_URL);
+		check(res, {
+			'status é 200': (r) => r.status === 200,
+			'tempo de resposta < 1000ms': (r) => r.timings.duration < 1000,
+		});
 	}
 
-	// Validações para garantir qualidade da resposta
-	check(res, {
-		'status é 200': (r) => r.status === 200,
-		'tempo de resposta < 500ms': (r) => r.timings.duration < 500,
-	});
-
-	// Simula intervalo entre as requisições
 	sleep(1);
 }
 

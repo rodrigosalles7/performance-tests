fe38567d1f343636af1950f574aaa98dd3f2368e
rodrigosalles7 <rodrigo.oliveira@scoder.com.br>
2025-02-17 14:36:47 -0300

    update repo

--- performance/loadTest.js
+++ performance/loadTest.js
@@ -4,14 +4,14 @@ import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporte
 
 const BASE_URL = __ENV.BASE_URL || 'https://api-hml.ilotto.com.br/carts/purchase';
 const REQUEST_TYPE = __ENV.REQUEST_TYPE || 'POST'; // Variável de ambiente para GET ou POST
-const POST_BODY = __ENV.POST_BODY || '{"raffleId":"ea65656e-7a6c-404c-afbc-7a5f233eff5c","ticketsAmount":1,"customerId":"020a1e1b-526b-416d-894c-ba6e7f2f5068","email":"rodrigo.oliveira@scoder.com.br","cpf":"12622755635","phone":"5535992027336","isFromBalance":false,"purchaseWithPromotion":false}'; // Corpo da requisição POST, em formato JSON
+// const POST_BODY = __ENV.POST_BODY || '{"raffleId":"ea65656e-7a6c-404c-afbc-7a5f233eff5c","ticketsAmount":1,"customerId":"020a1e1b-526b-416d-894c-ba6e7f2f5068","email":"rodrigo.oliveira@scoder.com.br","cpf":"12622755635","phone":"5535992027336","isFromBalance":false,"purchaseWithPromotion":false}'; // Corpo da requisição POST, em formato JSON
 // const POST_BODY = JSON.parse(__ENV.POST_BODY || '{"raffleId":"028574ba-34ea-42fb-9a4f-9c443dbf6abf","ticketsAmount":1,"customerId":"","name":"Rodrigo O. S.","email":"rodrigo.oliveira@scoder.com.br","cpf":"12622755635","phone":"5535992027336","birthDate":null,"purchaseWithPromotion":false}');
 const VIRTUAL_USERS_CONSTANT = __ENV.VIRTUAL_USERS_CONSTANT || 1;
 const VIRTUAL_USERS_RAMP_UP = __ENV.VIRTUAL_USERS_RAMP_UP || 1;
 const VIRTUAL_USERS_RAMP_DOWN = __ENV.VIRTUAL_USERS_RAMP_DOWN || 0;
-const TEST_DURATION_CONSTANT = __ENV.TEST_DURATION_CONSTANT || '2s';
-const TEST_DURATION_RAMP_UP = __ENV.TEST_DURATION_RAMP_UP || '2s';
-const TEST_DURATION_RAMP_DOWN = __ENV.TEST_DURATION_RAMP_DOWN || '2s';
+const TEST_DURATION_CONSTANT = __ENV.TEST_DURATION_CONSTANT || '1s';
+const TEST_DURATION_RAMP_UP = __ENV.TEST_DURATION_RAMP_UP || '1s';
+const TEST_DURATION_RAMP_DOWN = __ENV.TEST_DURATION_RAMP_DOWN || '1s';
 
 // Configuração de carga
 export const options = {
@@ -26,17 +26,19 @@ export const options = {
 	// },
 };
 
-const jsonData = open('./post_body.json', 'b');
+// const jsonData = open('./post_body.json');
+
+const jsonData = JSON.parse(open('./post_body.json'));
 
 export default function () {
 	let res;
 
 	if (REQUEST_TYPE.toUpperCase() === 'POST') {
 
-		res = http.post(BASE_URL, jsonData, { headers: { 'Content-Type': 'application/json' } });
+		res = http.post(BASE_URL, JSON.stringify(jsonData), { headers: { 'Content-Type': 'application/json' } });
 		console.log(res)
 		console.log('------------------------')
-		console.log(jsonData)
+		console.log(JSON.stringify(jsonData, null, 2))
 		console.log('------------------------')
 
 		check(res, {

fbf502fd15de81acf3cad12bae18ea9dbaf98fa7
rodrigosalles7 <rodrigo.oliveira@scoder.com.br>
2025-02-17 16:21:26 -0300

    update repo

--- performance/stressTest.js
+++ performance/stressTest.js
@@ -1,5 +1,5 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check, sleep } from 'k6';
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 const BASE_URL = __ENV.BASE_URL || 'https://api-hml2.ilotto.com.br';
